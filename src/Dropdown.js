ebe72dfd23d06fe9b3cfd84d8f80d03b354f1845
Snehal Patil <snehal.patil@Eternus.local>
2019-05-31 16:50:07 +0530

    initial setup

---
+++ src/Dropdown.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dropdown = props => {
+    return (
+        <div>
+            Dropdown
+            {/* Write code for dropdown component here */}
+        </div>
+    )
+}
+export {Dropdown};
